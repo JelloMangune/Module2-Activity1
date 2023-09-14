0c295e30a803e0ffde3453a8f4c1c679901fed94
JelloMangune <mangune.jello@auf.edu.ph>
2023-09-15 06:51:13 +0800

    first commit

---
+++ Module02_Activity01/Module02_Activity01/View/Page1.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Module02_Activity01.ViewModel;
+
+namespace Module02_Activity01.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+            BindingContext = new TaskViewModel();
+        }
+    }
+}
---
+++ Module02_Activity01/Module02_Activity01/ViewModel/TaskViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Module02_Activity01.Model;
+
+
+namespace Module02_Activity01.ViewModel
+{
+    class TaskViewModel
+    {
+        public Taskmodel TaskModelSet { get; set; }
+        public TaskViewModel()
+        {
+            TaskModelSet = new Taskmodel
+            {
+                SubjectCode = "PDC06",
+                SubjectTitle = "Xamarin Mobile Programming",
+                Unit = 3,
+            };
+        }
+    }
+}
+
